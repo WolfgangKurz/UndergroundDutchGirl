17bea6815882de8876f6dc839113df326ed46585
Wolfgang Kurz <wolfgangkurzdev@gmail.com>
2022-02-10 19:59:26 +0900

    wip1

--- app/src/main/java/com/swaytwig/undergrounddutchgirl/MainActivity.kt
+++ app/src/main/java/com/swaytwig/undergrounddutchgirl/MainActivity.kt
@@ -2,10 +2,37 @@ package com.swaytwig.undergrounddutchgirl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private val fragmentHome by lazy { HomeFragment() }
+    private val fragmentTexture by lazy { TextureFragment() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initNavigationBar()
+    }
+
+    private fun initNavigationBar() {
+        val navigation = findViewById<BottomNavigationView>(R.id.navigation)
+
+        navigation.run {
+            setOnItemSelectedListener {
+                when(it.itemId) {
+                    R.id.action_nav_home -> changeFragment(fragmentHome)
+                    R.id.action_nav_texture -> changeFragment(fragmentTexture)
+                }
+                true
+            }
+        }
+    }
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_container, fragment)
+            .commit()
     }
 }
